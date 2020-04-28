2446e87eca662de804632cddb95f086869fe6e4e
Ryan Xu <rxu@brandeis.edu>
2020-04-28 11:57:14 -0400

    Restart to incorperate BaseGame

---
+++ arena/lib_old/main_old.dart
@@ -0,0 +1,37 @@
+import 'package:flame/flame.dart';
+import 'package:flame/gestures.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'arena.dart';
+
+void main() async {
+  Util flame = Util();
+  WidgetsFlutterBinding.ensureInitialized();
+  await flame.fullScreen();
+  await flame.setOrientation(DeviceOrientation.portraitUp);
+
+  Flame.images.loadAll(<String>[
+    'bg/icy_bg.jpg',
+    'pieces/penguin.png',
+    'pieces/penguin2.png',
+  ]);
+  Arena game = Arena();
+  runApp(game.widget);
+
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
+  flame.addGestureRecognizer(tapper);
+
+  PanGestureRecognizer pan = PanGestureRecognizer();
+  pan.onStart = game.onPanStart;
+  pan.onUpdate = game.onDragUpdate;
+  pan.onEnd = game.onPanEnd;
+  flame.addGestureRecognizer(pan);
+  /*
+  DoubleTapGestureRecognizer doubleTap = DoubleTapGestureRecognizer();
+  doubleTap.onDoubleTap = game.startPieces;
+  flame.addGestureRecognizer(doubleTap);
+  */
+}
